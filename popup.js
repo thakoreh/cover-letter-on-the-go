9c8dff39069b47d7210ffe2b863c8c9d9d7153ce
Hiren Thakore <hiren.thakore58@gmail.com>
2023-06-27 21:05:28 -0400

    first commit

---
+++ popup.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const resumeInput = document.getElementById("resume");
+  const captureButton = document.getElementById("captureDescription");
+  const selectedDescription = document.getElementById("selectedDescription");
+  const generatedLetter = document.getElementById("generatedLetter");
+  const copyButton = document.getElementById("copyButton");
+
+  resumeInput.addEventListener("change", function () {
+    const file = this.files[0];
+    parseResume(file);
+  });
+
+  captureButton.addEventListener("click", function () {
+    // Capture the selected job description using cursor logic here
+    // Update the selectedDescription element with the captured text
+  });
+
+  copyButton.addEventListener("click", function () {
+    const textToCopy = generatedLetter.innerText;
+    // Copy the generated cover letter text to the clipboard
+    // Implement the copy functionality here
+  });
+
+  function parseResume(file) {
+    // Send the resume file to a server for parsing
+    // Update the server endpoint with your own implementation
+    const serverEndpoint = "https://your-server.com/parse-resume";
+
+    const formData = new FormData();
+    formData.append("resume", file);
+
+    fetch(serverEndpoint, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Process the parsed resume data
+        const resumeData = data;
+        generateCoverLetter(resumeData);
+      })
+      .catch((error) => {
+        console.error("Error parsing resume:", error);
+      });
+  }
+
+  function generateCoverLetter(resumeData) {
+    // Use the parsed resume data and selected job description
+    // to generate the cover letter using the ChatGPT API
+    // Update this code with your ChatGPT API integration
+    const jobDescription = selectedDescription.innerText;
+    const coverLetter =
+      "Generated cover letter based on the resume and job description";
+
+    // Display the generated cover letter on the UI
+    generatedLetter.innerText = coverLetter;
+  }
+});
